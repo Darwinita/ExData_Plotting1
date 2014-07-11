eb5dcdcafd1fc768623e94df2e2c2ef006608728
Darwinita <lcpaulet@gmail.com>
2014-07-11 12:03:43 -0400

    Uploading files
    (4 R scripts and 4 plots in png format)

---
+++ plot2.R
@@ -0,0 +1,22 @@
+#Set the working directory in your computer
+setwd("/Users/lorenzo/Documents/Johns\ Hopkins\ University\ Data\ Science/Exploratory_Data_Analysis/Assessment1/")
+
+fileURL<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+datafilename<-"Course_project.zip"
+#Check if the file containing the data is already in your working directory. Otherwise, download it and unzip
+if (!file.exists(datafilename)) { 
+  download.file(fileURL, destfile = datafilename, method = "curl")
+  unzip("Course_project.zip")
+}
+
+filename<-"household_power_consumption.txt"
+
+#Read data from file (Using skip and nrows to extract only data from 2007-02-01 and 2007-02-02)
+raw_data<-read.table(filename, sep = ";", header = T, na.strings = "?", comment.char="", skip = 66636, nrows = 2880,  as.is=T, stringsAsFactors=F, colClasses = , col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")) #,  na.strings = "?", strip.white = T, fill = T, blank.lines.skip = T
+raw_data$datetime <- paste(raw_data$Date,raw_data$Time, sep = ' ') 
+raw_data$datetime<-strptime(raw_data$datetime, "%d/%m/%Y %H:%M:%S")
+
+#Plotting
+png(file = "plot2.png")
+with(raw_data, plot(datetime, Global_active_power, type = "l", ylab="Global Active Power (kilowatts)", xlab=""))
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,34 @@
+#Set the working directory in your computer
+setwd("/Users/lorenzo/Documents/Johns\ Hopkins\ University\ Data\ Science/Exploratory_Data_Analysis/Assessment1/")
+
+fileURL<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+datafilename<-"Course_project.zip"
+#Check if the file containing the data is already in your working directory. Otherwise, download it and unzip
+if (!file.exists(datafilename)) { 
+  download.file(fileURL, destfile = datafilename, method = "curl")
+  unzip("Course_project.zip")
+}
+
+filename<-"household_power_consumption.txt"
+
+#Read data from file (Using skip and nrows to extract only data from 2007-02-01 and 2007-02-02)
+raw_data<-read.table(filename, sep = ";", header = T, na.strings = "?", comment.char="", skip = 66636, nrows = 2880,  as.is=T, stringsAsFactors=F, colClasses = , col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")) #,  na.strings = "?", strip.white = T, fill = T, blank.lines.skip = T
+raw_data$datetime <- paste(raw_data$Date,raw_data$Time, sep = ' ') 
+raw_data$datetime<-strptime(raw_data$datetime, "%d/%m/%Y %H:%M:%S")
+
+#Plotting
+png(file = "plot4.png")
+par(mfrow = c(2, 2))
+with(raw_data, {
+  plot(datetime, Global_active_power, type = "l", ylab="Global Active Power", xlab="")
+  plot(datetime, Voltage, type = "l", ylab="Voltage")
+  plot(raw_data$datetime, raw_data$Sub_metering_1, 
+       type = "l", 
+       ylab = "Energy sub metering",
+       xlab="")
+  lines(raw_data$datetime, raw_data$Sub_metering_2, col = "red")
+  lines(raw_data$datetime, raw_data$Sub_metering_3, col = "blue")
+  legend("topright", col = c("black", "blue", "red"), bg=NULL,box.lwd = 0, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1))
+  plot(datetime, Global_reactive_power, type = "l", ylim=c(0, 0.5))
+})
+dev.off()
